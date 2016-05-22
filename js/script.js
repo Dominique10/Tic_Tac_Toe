5e4d8c3ca07a907bc21ee86807b58db42d398a68
cssdominique <dominique@christiansteven.com>
2016-05-22 01:26:01 -0400

    Added check for winner function, and new game button

--- js/script.js
+++ js/script.js
@@ -34,36 +34,65 @@ function spaceOpen(selection,value){
 }
 
 //function to check if player has won
-// function winnerCheck(){
-// 	if ((document.getElementById('s1').innerHTML==document.getElementById('s2').innerHTML) && (document.getElementById('s2').innerHTML==document.getElementById('s3').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 	else if ((document.getElementById('s4').innerHTML==document.getElementById('s5').innerHTML) && (document.getElementById('s5').innerHTML==document.getElementById('s6').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 	else if ((document.getElementById('s7').innerHTML==document.getElementById('s8').innerHTML) && (document.getElementById('s8').innerHTML==document.getElementById('s8').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 	else if ((document.getElementById('s1').innerHTML==document.getElementById('s4').innerHTML) && (document.getElementById('s4').innerHTML==document.getElementById('s7').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 	else if ((document.getElementById('s2').innerHTML==document.getElementById('s5').innerHTML) && (document.getElementById('s5').innerHTML==document.getElementById('s8').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 	else if ((document.getElementById('s3').innerHTML==document.getElementById('s6').innerHTML) && (document.getElementById('s6').innerHTML==document.getElementById('s9').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 	else if ((document.getElementById('s1').innerHTML==document.getElementById('s5').innerHTML) && (document.getElementById('s5').innerHTML==document.getElementById('s9').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// 		else if ((document.getElementById('s3').innerHTML==document.getElementById('s5').innerHTML) && (document.getElementById('s5').innerHTML==document.getElementById('s7').innerHTML)) {
-// 		alert("We have a winner");
-// 	}
-// }
+function winnerCheck(){
+	if ((document.getElementById('s1').innerHTML=="X") && (document.getElementById('s2').innerHTML=="X") && (document.getElementById('s3').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s4').innerHTML=="X") && (document.getElementById('s5').innerHTML=="X") && (document.getElementById('s6').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s4').innerHTML=="X") && (document.getElementById('s5').innerHTML=="X") && (document.getElementById('s6').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s7').innerHTML=="X") && (document.getElementById('s8').innerHTML=="X") && (document.getElementById('s9').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s1').innerHTML=="X") && (document.getElementById('s4').innerHTML=="X") && (document.getElementById('s7').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s2').innerHTML=="X") && (document.getElementById('s5').innerHTML=="X") && (document.getElementById('s8').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s3').innerHTML=="X") && (document.getElementById('s6').innerHTML=="X") && (document.getElementById('s9').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s3').innerHTML=="X") && (document.getElementById('s5').innerHTML=="X") && (document.getElementById('s7').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s1').innerHTML=="X") && (document.getElementById('s5').innerHTML=="X") && (document.getElementById('s9').innerHTML=="X")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s1').innerHTML=="O") && (document.getElementById('s2').innerHTML=="O") && (document.getElementById('s3').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s4').innerHTML=="O") && (document.getElementById('s5').innerHTML=="O") && (document.getElementById('s6').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s4').innerHTML=="O") && (document.getElementById('s5').innerHTML=="O") && (document.getElementById('s6').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s7').innerHTML=="O") && (document.getElementById('s8').innerHTML=="O") && (document.getElementById('s9').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s1').innerHTML=="O") && (document.getElementById('s4').innerHTML=="O") && (document.getElementById('s7').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s2').innerHTML=="O") && (document.getElementById('s5').innerHTML=="O") && (document.getElementById('s8').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s3').innerHTML=="O") && (document.getElementById('s6').innerHTML=="O") && (document.getElementById('s9').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s3').innerHTML=="O") && (document.getElementById('s5').innerHTML=="O") && (document.getElementById('s7').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+	else if ((document.getElementById('s1').innerHTML=="O") && (document.getElementById('s5').innerHTML=="O") && (document.getElementById('s9').innerHTML=="O")) {
+		alert("We have a winner");
+	}
+ }
 //function to start new game
 function startNewGame(){
-	//$("#board > tbody").html("");
-	$('#board').find('tr').each(function (i, value) {
-    console.log(i+' '+value);
-    });
+	$("td").each(function() {
+    $(this).text('');
+  });
 }
